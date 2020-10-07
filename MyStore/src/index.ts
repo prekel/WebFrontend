ef331388f557d76034297c6d89b45e2f36662a52
Vladislav Prekel <misterptits@yandex.ru>
2020-10-07 20:55:08 +0700

    asdd

--- MyStore/src/index.ts
+++ MyStore/src/index.ts
@@ -3,7 +3,7 @@ import * as _ from 'lodash';
 function component() {
     const element = document.createElement('div');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello', 'webpack1'], ' ');
 
     return element;
 }
