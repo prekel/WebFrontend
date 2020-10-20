06564547e0440856b9750473ed4d9d389c2ec56c
Vladislav Prekel <misterptits@yandex.ru>
2020-10-20 21:31:21 +0700

    reports

--- Pr_06/public/js/funcs.js
+++ Pr_06/public/js/funcs.js
@@ -1,20 +1,20 @@
 import * as tasks from "./tasks.js";
 function task1(x) {
-    var a = document.getElementById("answer");
+    let a = document.getElementById("answer");
     if (a != null) {
-        a.innerHTML = tasks.arraySum(x.split(" ").map(function (n) { return parseInt(n); })).toString();
+        a.innerHTML = tasks.arraySum(x.split(" ").map(n => parseInt(n))).toString();
     }
 }
 function task8(x, y) {
-    var a = document.getElementById("answer");
+    let a = document.getElementById("answer");
     if (a != null) {
-        a.innerHTML = tasks.zipEqualCount(x.split(" ").map(function (n) { return parseInt(n); }), y.split(" ").map(function (n) { return parseInt(n); })).toString();
+        a.innerHTML = tasks.zipEqualCount(x.split(" ").map(n => parseInt(n)), y.split(" ").map(n => parseInt(n))).toString();
     }
 }
 function task26(x) {
-    var a = document.getElementById("answer");
+    let a = document.getElementById("answer");
     if (a != null) {
-        a.innerHTML = tasks.MinMax.differenceMaxMin(x.split(" ").map(function (n) { return parseInt(n); })).toString();
+        a.innerHTML = tasks.MinMax.differenceMaxMin(x.split(" ").map(n => parseInt(n))).toString();
     }
 }
 window.task1 = task1;
--- mystore/src/components/ProductCard.tsx
+++ mystore/src/components/ProductCard.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export const ProductCard: React.FunctionComponent<{
   msg: string;
@@ -8,16 +10,18 @@ export const ProductCard: React.FunctionComponent<{
 
   return (
     <div className="input">
-      <input type="text" id="title"></input>
+      <input type="text"  className="form-control" id="title"/>
       <label htmlFor="title" className="active">
         {msg}
       </label>
-      <button onClick={() => onClick()}></button>
+      <br/>
+      <button type="button" 
+        className="btn btn-danger" onClick={() => onClick()}>qasdas</button>
       {n}
-      <button
-        className="btn-primary"
+      <button type="button"
+        className="btn btn-primary"
         onClick={(event: React.MouseEvent) => setN(event.button)}
-      ></button>
+      >Qwertyt1</button>
     </div>
   );
 };
